30317a55b65df3a6f249c5c55aa46c4050708afb
Amin Boukari <ambouk3@gmail.com>
2020-06-08 12:32:42 -0400

    Added ICA and top 20 k score + pop density graph

---
+++ ica.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import sklearn as sk
+from sklearn.decomposition import FastICA
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
+print("Loading CSV Data")
+sef_data = pd.read_csv("data/SEF_filled_mean.csv", dtype={"cty": str, "name": str, "_merge": str, "_merge1": str, "_merge2": str}, index_col=0)
+
+data_values = sef_data.loc[:, 'median_rent2016':'has_mom_rh_gp_p50_l']
+print(data_values)
+
+data_values = StandardScaler().fit_transform(data_values)
+
+ica = FastICA()
+
+ica_output = ica.fit_transform(data_values)
+
+sef_ica = pd.DataFrame(data = ica_output, index=sef_data.index)
+
+print(sef_ica)
+
+sef_ica.to_csv("data/SEF_ICA.csv")
+sef_ica.to_excel("data/SEF_ICA.xlsx", sheet_name='SEF ICA Output')  
+
+# plot = sef_pca.plot.scatter(x='Principal component 1', y='Principal component 2')
+
+# plt.show()
