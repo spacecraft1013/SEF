6c68973e9cc96af339e0afd96c8a591666740385
Amin Boukari <ambouk3@gmail.com>
2020-06-20 04:26:27 -0400

    Fixed some issues with multivariate imputing

--- mulivariate_imputing.py
+++ mulivariate_imputing.py
@@ -10,8 +10,9 @@ data_values = sef_data.loc[:, 'median_rent2016':'has_mom_rh_gp_p50_l']
 print("Filling Values using Multivariate Imputation")
 imp = IterativeImputer(verbose=2)
 data_values_imputed = imp.fit_transform(data_values)
+data_values_imputed = pd.DataFrame(data_values_imputed, columns=data_values.columns)
 sef_data.replace(data_values, data_values_imputed)
 
 print("Saving Output")
 sef_data.to_csv("data/SEF_filled_imputed.csv")
-sef_data.to_excel("data/SEF_filled_imputed.xlsx", sheet_name='SEF Data')
+sef_data.to_excel("data/SEF_filled_imputed.xlsx", sheet_name='SEF Data Imputed')
