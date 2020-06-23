14b8fb204d6180f4175d856e1730bd979a12987b
Amin Boukari <ambouk3@gmail.com>
2020-06-23 11:59:07 -0400

    Testing with KNN

--- mulivariate_imputing.py
+++ mulivariate_imputing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
+from impyute.imputation.cs import fast_knn
+import sys
 
 print("Loading CSV Data")
 sef_data = pd.read_csv("data/SEF_filtered.csv", index_col=0)
@@ -8,8 +10,9 @@ sef_data = pd.read_csv("data/SEF_filtered.csv", index_col=0)
 data_values = sef_data.loc[:, 'median_rent2016':'has_mom_rh_gp_p50_l']
 
 print("Filling Values using Multivariate Imputation")
-imp = IterativeImputer(verbose=2)
-data_values_imputed = imp.fit_transform(data_values)
+# imp = IterativeImputer(verbose=2)
+# data_values_imputed = imp.fit_transform(data_values)
+data_values_imputed = fast_knn(data_values.values, verbose=1)
 data_values_imputed = pd.DataFrame(data_values_imputed, columns=data_values.columns)
 sef_data.replace(data_values, data_values_imputed)
 
